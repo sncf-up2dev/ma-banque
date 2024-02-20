0241a95b8fd0f671f026d75c5e09356d16a63269
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-02-20 17:17:47 +0100

    Use finally to close scanner

--- src/main/java/fr/sncf/banque/Main.java
+++ src/main/java/fr/sncf/banque/Main.java
@@ -16,25 +16,29 @@ public class Main {
         monCompte.crediter(500);
 
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Veuillez entrer le code iban de destinataire, ou \"exit\" pour sortir.");
-            String iban = scanner.nextLine(); // Lecture code iban
-            if (iban.equalsIgnoreCase("exit")) {
-                break;
+        try {
+            while (true) {
+                System.out.println("Veuillez entrer le code iban de destinataire, ou \"exit\" pour sortir.");
+                String iban = scanner.nextLine(); // Lecture code iban
+                if (iban.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                System.out.println("Veuillez entrer un montant.");
+                String montantStr = scanner.nextLine();
+                try {
+                    double montant = Double.parseDouble(montantStr);
+                    monCompte.faireVirement(iban, montant);
+                } catch (NumberFormatException e) {
+                    System.err.println("Le montant doit être une valeur réelle !");
+                } catch (CompteNonTrouvableException e) {
+                    System.err.println("Compte non trouvé !");
+                } catch (SoldeNonSuffisanteException e) {
+                    System.err.println("Solde non suffisante !");
+                }
+                CompteBancaire.printAll();
             }
-            System.out.println("Veuillez entrer un montant.");
-            String montantStr = scanner.nextLine();
-            try {
-                double montant = Double.parseDouble(montantStr);
-                monCompte.faireVirement(iban, montant);
-            } catch (NumberFormatException e) {
-                System.err.println("Le montant doit être une valeur réelle !");
-            } catch (CompteNonTrouvableException e) {
-                System.err.println("Compte non trouvé !");
-            } catch (SoldeNonSuffisanteException e) {
-                System.err.println("Solde non suffisante !");
-            }
-            CompteBancaire.printAll();
+        } finally {
+            scanner.close();
         }
     }
 }
